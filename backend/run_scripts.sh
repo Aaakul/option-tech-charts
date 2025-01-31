bb596368887a23601f4db4d43d00a561bc00cc26
Aaakul <lukajiangg@gmail.com>
2025-02-01 01:03:00 +0800

    modify shell script

--- backend/run_scripts.sh
+++ backend/run_scripts.sh
@@ -7,4 +7,7 @@ for script in get_csv.py get_current_month.py get_summary.py get_filtered.py get
         exit 1
     fi
 done
+
+rm -rf ../frontend/option-tech-charts/public/data/*  
+cp -r ./JSON/* ../frontend/option-tech-charts/public/data/
 echo "All scripts have been executed successfully."
